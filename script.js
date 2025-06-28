c9864c426f4573e727099c8b264c7b9bb3a8041c
UsmanSalah <salahidinovusman3@gmail.com>
2025-06-28 15:59:25 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('spin').addEventListener('click', function () {
-    const wheel = document.getElementById('wheel');
-    const deg = Math.floor(5000 + Math.random() * 5000);
-    wheel.style.transition = 'transform 5s ease-out';
-    wheel.style.transform = `rotate(${deg}deg)`;
-});
